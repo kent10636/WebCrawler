0da1a5e877d90ff17fba02f66d78ad44f622bd65
Kent <767188747@qq.com>
2018-04-17 23:19:14 +0800

    PhantomJS & Selenium

---
+++ PhantomJS/music.js
@@ -0,0 +1,10 @@
+var page = require('webpage').create();
+page.open('http://music.163.com/', function(status)
+{
+	console.log("Status: " + status);
+	if(status === "success") 
+	{
+		page.render('music.png');
+	}
+	phantom.exit();
+});
---
+++ Selenium/Selenium.py
@@ -0,0 +1,16 @@
+# 不知为何无法在搜索框中自动输入并搜索
+from selenium import webdriver  # 导入Selenium的webdriver
+from selenium.webdriver.common.keys import Keys  # 导入Keys
+
+driver = webdriver.Chrome()  # 指定使用的浏览器，初始化webdriver
+driver.get("http://www.python.org")  # 请求网页地址
+assert "Python" in driver.title  # 看看Python关键字是否在网页title中，如果在则继续，如果不在，程序跳出。
+elem = driver.find_element_by_name("q")  # 找到name为q的元素，这里是个搜索框
+elem.clear()  # 清空搜索框中的内容
+elem.send_keys("pycon")  # 在搜索框中输入pycon
+elem.send_keys(Keys.RETURN)  # 相当于回车键，提交
+assert "No results found." not in driver.page_source  # 如果当前页面文本中有“No results found.”则程序跳出
+driver.close()  # 关闭webdriver
+
+# ChromeDriver启动Chrome浏览器后，地址栏只显示data;，说明chromeDriver版本不对，
+# 需要下载对应版本的 webdriver，网址为：https://sites.google.com/a/chromium.org/chromedriver/downloads
